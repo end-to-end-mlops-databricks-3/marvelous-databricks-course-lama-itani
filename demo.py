c8b087df6208cf1f156d9aa3193e0f9dda1e9973
Maria Vechtomova <130861802+mvechtomova@users.noreply.github.com>
2025-05-10 09:59:32 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,8 +4,4 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 df = spark.read.table("samples.nyctaxi.trips")
 df.show(5)
-<<<<<<< HEAD
 # COMMAND ----------
-=======
-# COMMAND ----------
->>>>>>> 08790bf (bugfix)
